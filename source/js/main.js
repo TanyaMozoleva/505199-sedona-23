96b1e47c0d70fb8ccfa30cc551e4d129842e65ce
TanyaMozoleva <37499067+TanyaMozoleva@users.noreply.github.com>
2021-09-14 01:39:24 +0300

    fixing pp

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,7 @@
+const menuBtn = document.querySelector(".main-nav__toggle");
+const menuList = document.querySelector(".main-nav__list");
+const navigation = document.querySelector(".main-nav");
 
-
-
-//main-nav
-const menuBtn = document.querySelector("main-nav__toggle");
-const menuList = document.querySelector("main-nav__list");
-const navigation = document.querySelector("main-nav");
-closeMenu();
 
 menuList.classList.remove("main-nav__list--nojs");
 navigation.classList.remove("main-nav--nojs");
@@ -13,15 +9,18 @@ menuBtn.classList.remove("main-nav__toggle--nojs");
 
   menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (menuBtn.classList.contains("main-nav__toggle--active") || menuList.classList.contains("main-nav__list--opened")) {
+    if (menuBtn.classList.contains("main-nav__toggle--opened") || menuList.classList.contains("main-nav__list--opened")) {
       closeMenu();
-    } else openMenu();
+    } else { openMenu();
+    }
   });
   function openMenu() {
     menuBtn.classList.add("main-nav__toggle--opened");
-    menuList.classList.add("main-nav__list--opend");
+    menuList.classList.add("main-nav__list--opened");
   }
   function closeMenu() {
     menuBtn.classList.remove("main-nav__toggle--opened");
     menuList.classList.remove("main-nav__list--opened");
   }
+
+
